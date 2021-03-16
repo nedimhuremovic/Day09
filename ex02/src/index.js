ed7b46f6aa5d937ef6b846e5cf827da4a98a29f4
nedimhuremovic <nedimhuremovic@gmail.com>
2021-03-16 15:42:05 +0100

    ex02 added

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+    function myFunction(myParameter) {
+        if(myParameter)
+        {
+            return "The parameter is true!";
+        }
+        return "The paremeter is false!";
+    }
+    console.log(myFunction(false));
+// Only change code bellow this line
+module.exports = myFunction;
