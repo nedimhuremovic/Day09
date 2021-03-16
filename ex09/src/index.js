561a04b8d633d031810177cd5414a8882c9413eb
nedimhuremovic <nedimhuremovic@gmail.com>
2021-03-16 19:00:28 +0100

    ex09 added

---
+++ ex09/src/index.js
@@ -0,0 +1,12 @@
+// Only change code below this line
+ function functionElse(num) {
+     var result;
+     if(num<=5)
+     result = "5 or smaller";
+     else 
+     result = "Bigger than 5";
+     return result;
+ }
+    console.log(functionElse(30));
+// Only change code bellow this line
+module.exports = functionElse;
