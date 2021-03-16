2d08daaa220c6e5868985572d404704bc7a3bd89
nedimhuremovic <nedimhuremovic@gmail.com>
2021-03-16 21:42:35 +0100

    ex16 added

---
+++ ex16/src/index.js
@@ -0,0 +1,8 @@
+// Only change code below this line
+    function checkSign(num) {
+        return num === 0 ? "zero" : num < 0 ? "negative" : "positive";
+    }
+    console.log(checkSign(-10));
+  // Only change code above this line
+  module.exports = checkSign;
+  
