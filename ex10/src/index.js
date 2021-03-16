3dfc4fa21d4b3fa7035ecd931c495ce77b4a3538
nedimhuremovic <nedimhuremovic@gmail.com>
2021-03-16 19:09:23 +0100

    ex10 added

---
+++ ex10/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+function logicOrder(num) {
+    if (num < 50) {
+        return "Less than 50";
+    } else if (num < 100) {
+        return "Less than 100";
+    } else {
+        return "Greater than or equal to 100";
+    } 
+}
+console.log(logicOrder(164));
+// Only change code bellow this line
+module.exports = logicOrder;
