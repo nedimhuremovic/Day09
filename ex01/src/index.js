e4b3cca2d06eb29dd7b269e2f4f97d136ea6635b
nedimhuremovic <nedimhuremovic@gmail.com>
2021-03-16 15:30:56 +0100

    ex01 added

---
+++ ex01/src/index.js
@@ -0,0 +1,7 @@
+// Only change code below this line
+    function myTrueBoolean() {
+        return true;
+    }
+    console.log(myTrueBoolean());
+// Only change code bellow this line
+module.exports = myTrueBoolean;
