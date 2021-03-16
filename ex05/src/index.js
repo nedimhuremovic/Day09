c5ca90caa094641c8c730a72b943cbdd7c3fdef5
nedimhuremovic <nedimhuremovic@gmail.com>
2021-03-16 18:28:06 +0100

    ex05 added

---
+++ ex05/src/index.js
@@ -0,0 +1,9 @@
+// Only change code below this line
+function compareStrictValues(m) {
+    if(m != 55)
+    return "Not equal";
+    return "Equal";
+}
+console.log(compareStrictValues("55"));
+// Only change code bellow this line
+module.exports = compareStrictValues;
