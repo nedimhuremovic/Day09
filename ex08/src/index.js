1d4892eb71f3c09517ed59e2adf6819cab5803fb
nedimhuremovic <nedimhuremovic@gmail.com>
2021-03-16 18:55:09 +0100

    ex08 added

---
+++ ex08/src/index.js
@@ -0,0 +1,9 @@
+// Only change code below this line
+    function logicalOrOperator(num) {
+        if(num<20 || num >30)
+        return "Out";
+        return "In";
+    }
+    console.log(logicalOrOperator(10));
+// Only change code bellow this line
+module.exports = logicalOrOperator;
