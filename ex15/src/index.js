c7ed88aad3dbe8d7cf6e4823cc2a6fe093bfee47
nedimhuremovic <nedimhuremovic@gmail.com>
2021-03-16 21:35:14 +0100

    ex15 added

---
+++ ex15/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+function myDoWhile() {
+  var i = 0;
+  var myNumbers = "";
+  do {
+    myNumbers += i;
+    if (i !== 9) myNumbers += ", ";
+    i++;
+  } while (i < 10);
+  return myNumbers;
+}
+
+console.log(myDoWhile());
+// Only change code above this line
+module.exports = myDoWhile;
